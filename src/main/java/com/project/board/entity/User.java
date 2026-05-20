e69983d2166d272ef37c6ca9f96903050327d846
gogulu <dondonsina@yahoo.co.jp>
2026-05-20 23:01:46 +0900

    fixed

--- src/main/java/com/project/board/entity/User.java
+++ src/main/java/com/project/board/entity/User.java
@@ -23,7 +23,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(name="created_at")
+    @Column(name="created_at",nullable = false)
     private LocalDateTime createdAt;
 
     protected User() {
